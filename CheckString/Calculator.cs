e593c9a1cb1f887b16435fdf6d774d9dc2822897
Mariya Baturina <baturinamariyawork.gmail.com>
2021-06-14 15:48:39 +0300

    add all files

---
+++ CheckString/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckString
+{
+   public class Calculator
+    {
+        public static int GetMaxIndexesDifference(List<int> indexes)
+        {
+            int max = indexes.ElementAt(0);
+
+            for (int i = 0; i < indexes.Count - 1; i++)
+            {
+                int difference = indexes[i + 1] - indexes[i];
+
+                if (difference > max)
+                {
+                    max = difference;
+                }
+            }
+
+            return max;
+        }
+
+        public static List<int> GetIndexesOfRepeatableFollowingChars(char[] arrayOfUserStringChars)
+        {
+            List<int> indexes = new List<int>();
+
+            for (int i = 0; i < arrayOfUserStringChars.Length - 1; i++)
+            {
+                if (i == 0)
+                {
+                    indexes.Add(i);
+                }
+
+                if (arrayOfUserStringChars[i] == arrayOfUserStringChars[i + 1])
+                {
+                    indexes.Add(i + 1);
+                }
+            }
+
+            return indexes;
+        }
+    }
+}
---
+++ CheckString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {          
+            string userString = args[0];
+
+            char[] arrayOfUserStringChars = userString.ToCharArray();
+
+            List<int> userStringIndexesOfRepeatableFollowingChars = Calculator.GetIndexesOfRepeatableFollowingChars(arrayOfUserStringChars);
+
+            int maxIndexesDifference = Calculator.GetMaxIndexesDifference(userStringIndexesOfRepeatableFollowingChars);
+
+            Console.WriteLine(maxIndexesDifference);
+        }
+    }
+}
